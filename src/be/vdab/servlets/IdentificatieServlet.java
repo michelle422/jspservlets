f699309e6f38b5f018c157939373394233adaac0
cheli <cheli@192.168.0.155>
2017-12-29 22:59:03 +0100

    commit met sessions

--- src/be/vdab/servlets/IdentificatieServlet.java
+++ src/be/vdab/servlets/IdentificatieServlet.java
@@ -1,15 +1,13 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class IdentificatieServlet
@@ -18,21 +16,29 @@ import javax.servlet.http.HttpServletResponse;
 public class IdentificatieServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/JSP/identificatie.jsp";
-	private static final int COOKIE_MAXIMUM_LEEFTIJD =
-			60 /* seconden */ * 30 /* minuten */;
+//	private static final int COOKIE_MAXIMUM_LEEFTIJD =
+//			60 /* seconden */ * 30 /* minuten */;
 	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (request.getCookies() != null) {
-			for (Cookie cookie : request.getCookies()) {
-				if ("gebruikersnaam".equals(cookie.getName())) {
-					request.setAttribute("gebruikersnaam", URLDecoder.decode(cookie.getValue(), "UTF-8"));
-					break;
-				}
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			String gebruikersnaam = (String) session.getAttribute("gebruikersnaam");
+			if (gebruikersnaam != null) {
+				request.setAttribute("gebruikersnaam", gebruikersnaam);
 			}
 		}
+		
+//		if (request.getCookies() != null) {
+//			for (Cookie cookie : request.getCookies()) {
+//				if ("gebruikersnaam".equals(cookie.getName())) {
+//					request.setAttribute("gebruikersnaam", URLDecoder.decode(cookie.getValue(), "UTF-8"));
+//					break;
+//				}
+//			}
+//		}
 		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 
@@ -41,10 +47,14 @@ public class IdentificatieServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		Cookie cookie = new Cookie("gebruikersnaam", URLEncoder.encode(request.getParameter("gebruikersnaam"), "UTF-8"));
-		cookie.setMaxAge(COOKIE_MAXIMUM_LEEFTIJD);
-		response.addCookie(cookie);
-		response.sendRedirect(request.getRequestURI());
+		HttpSession session = request.getSession();
+		session.setAttribute("gebruikersnaam", request.getParameter("gebruikersnaam"));
+//		Cookie cookie = new Cookie("gebruikersnaam", URLEncoder.encode(request.getParameter("gebruikersnaam"), "UTF-8"));
+//		cookie.setMaxAge(COOKIE_MAXIMUM_LEEFTIJD);
+//		response.addCookie(cookie);
+//		response.sendRedirect(request.getRequestURI());
+		
+		response.sendRedirect(response.encodeRedirectURL(request.getRequestURI()));
 	}
 
 }

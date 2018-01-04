240f43b09157b5069489e8d09b884dcfe26398bf
cheli <cheli@192.168.0.155>
2018-01-04 16:33:38 +0100

    commit met listener

---
+++ src/be/vdab/listeners/MandjeListener.java
@@ -0,0 +1,56 @@
+package be.vdab.listeners;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+/**
+ * Application Lifecycle Listener implementation class MandjeListener
+ *
+ */
+@WebListener
+public class MandjeListener implements ServletContextListener, HttpSessionAttributeListener {	
+	private static final String MANDJE = "mandje";
+	private static final String AANTAL_MANDJES = "aantalMandjes";
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent event)  { 
+         event.getServletContext().setAttribute(AANTAL_MANDJES, new AtomicInteger());
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent event)  { 
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
+     */
+    public void attributeAdded(HttpSessionBindingEvent event)  { 
+         if (MANDJE.equals(event.getName())) {
+        	 ((AtomicInteger)event.getSession().getServletContext().getAttribute(AANTAL_MANDJES)).incrementAndGet();
+         }
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
+     */
+    public void attributeRemoved(HttpSessionBindingEvent event)  { 
+    	if (MANDJE.equals(event.getName())) {
+       	 ((AtomicInteger)event.getSession().getServletContext().getAttribute(AANTAL_MANDJES)).decrementAndGet();
+        }
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
+     */
+    public void attributeReplaced(HttpSessionBindingEvent arg0)  { 
+    }
+}
